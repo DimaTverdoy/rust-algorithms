82bf71d903ee59a01b48be6d4a5f283b0cb067f1
DimaTverdoy <tverdoy.dima.work@gmail.com>
2022-03-19 18:52:01 +0300

    Add algorithm fibonacci with large values and refactor other features

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,13 @@ mod test;
 fn main() {
     let mut x = [1, 43, 52, 5];
 
-    println!("Fibonacci result is {}", fibonacci::fibonacci(4));
-    println!("Fibonacci result is {}", fibonacci::fibonacci2(4));
+    println!("Fibonacci result is {}", fibonacci::fibonacci(1));
+    println!("Fibonacci result is {}", fibonacci::fibonacci2(3));
+    println!("Big fibonacci result is {}", fibonacci::big_fibonacci(4));
 
     sort::count::sort(&mut x);
     println!("Count sort result is {:?}", x);
+
 
     println!("Linear seach result is {}", search::linear::search(&x, &5).unwrap());
     println!("Binary seach result is {}", search::binary::search(&x, &5).unwrap());
