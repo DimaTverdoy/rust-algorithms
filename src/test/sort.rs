74576e444f17a2d351eb9cb7b704682584421cef
DimaTverdoy <tverdoy.dima.work@gmail.com>
2022-04-14 22:29:55 +0300

    Add test for binary search

--- src/test/sort.rs
+++ src/test/sort.rs
@@ -10,3 +10,16 @@ mod count {
         assert_eq!([0, 0, 1, 1, 4, 65, 8543], x)
     }
 }
+
+#[cfg(test)]
+mod insert {
+    use crate::sort::insert::sort;
+
+    #[test]
+    fn test() {
+        let mut x = [4, 1, 65, 1, 0, 0, 8543];
+
+        sort(&mut x);
+        assert_eq!([0, 0, 1, 1, 4, 65, 8543], x)
+    }
+}
