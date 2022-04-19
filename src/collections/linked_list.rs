c8fe3e64f96eebd83c83d2f3d0163bfd197f421f
DimaTverdoy <tverdoy.dima.work@gmail.com>
2022-04-19 22:02:40 +0300

    Update linked list

--- src/collections/linked_list.rs
+++ src/collections/linked_list.rs
@@ -1,21 +1,36 @@
+use std::cmp::Ordering;
+use std::fmt;
+use std::fmt::Formatter;
 use std::marker::PhantomData;
 use std::ptr::NonNull;
 
-#[derive(Debug)]
-pub struct LinkedList<T> {
+struct LinkedList<T> {
     head: Option<NonNull<Node<T>>>,
-    len: usize
+    tail: Option<NonNull<Node<T>>>,
+    len: usize,
+    marker: PhantomData<Option<Box<Node<T>>>>,
 }
 
-#[derive(Debug)]
 struct Node<T> {
+    previous: Option<NonNull<Node<T>>>,
     next: Option<NonNull<Node<T>>>,
-    element: T
+    element: T,
+}
+
+struct Iter<'a, T: 'a> {
+    head: Option<NonNull<Node<T>>>,
+    tail: Option<NonNull<Node<T>>>,
+    len: usize,
+    marker: PhantomData<&'a Node<T>>,
+}
+
+struct IntoIter<T> {
+    list: LinkedList<T>
 }
 
 impl<T> Node<T> {
     fn new(element: T) -> Self {
-        Self {next: None, element}
+        Self { previous: None, next: None, element }
     }
 
     fn into_element(self: Box<Self>) -> T {
@@ -23,115 +38,95 @@ impl<T> Node<T> {
     }
 }
 
-// Public methods
-impl<T> LinkedList<T> where T: std::cmp::PartialEq {
-    pub fn push(&mut self, element: T) {
-        self.push_back_node(box Node::new(element))
-    }
 
-    pub fn len(&self) -> usize {
-        self.len
-    }
-
-    pub fn get(&self, index: usize) -> Option<Box<T>> {
-        unsafe {
-            let node = self.get_node(index)?;
-            Some(box Box::from_raw(node.as_ptr()).into_element())
-        }
-    }
-
-    pub fn remove(&mut self, index: usize) {
-        self.remove_node(index)
-    }
-
-    pub fn index(&self, element: T) -> Option<usize> {
-        self.index_node(element)
-    }
-
-    pub fn first(&self) -> Option<Box<T>> {
-        self.get(0)
-    }
-
-    pub fn last(&self) -> Option<Box<T>> {
-        self.get(self.len() - 1)
-    }
-
-    pub fn is_empty(&self) -> bool {
-        self.head.is_none()
+impl<T: fmt::Debug> fmt::Debug for LinkedList<T> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        f.debug_list().entries(self).finish()
     }
 }
 
-/// Private methods
-impl<T> LinkedList<T> where T: std::cmp::PartialEq {
-    pub fn new() -> Self {
-        Self {head: None, len: 0}
+// Public methods
+impl<T> LinkedList<T> {
+    pub fn push_back(&mut self, element: T) {
+        self.push_back_node(box Node::new(element))
     }
+
+    pub fn push_front(&mut self, element: T) {
+        self.push_front_node(box Node::new(element))
+    }
+
+    pub fn iter(&self) -> Iter<'_, T> {
+        Iter {
+            head: self.head,
+            tail: self.tail,
+            len: self.len,
+            marker: PhantomData
+        }
+    }
+}
+
+// Private methods
+impl<T> LinkedList<T> {
+    #[inline]
     fn push_back_node(&mut self, mut node: Box<Node<T>>) {
         unsafe {
             node.next = None;
+            node.previous = self.tail;
+
+            let node = Some(Box::leak(node).into());
+
+            match self.tail {
+                None => self.head = node,
+                Some(tail_node) => (*tail_node.as_ptr()).next = node
+            }
+
+            self.tail = node;
+            self.len += 1;
+        }
+    }
+
+    #[inline]
+    pub fn push_front_node(&mut self, mut node: Box<Node<T>>) {
+        unsafe {
+            node.previous = None;
+            node.next = self.head;
+
             let node = Some(Box::leak(node).into());
 
             match self.head {
-                Some(_) => (*self.get_node(self.len() - 1).unwrap().as_ptr()).next = node,
-                None => self.head = node
+                None => self.tail = node,
+                Some(head_node) => (*head_node.as_ptr()).previous = node
             }
 
-            self.len += 1
+            self.head = node;
+            self.len += 1;
         }
     }
+}
 
-    fn get_node(&self, index: usize) -> Option<NonNull<Node<T>>> {
-        assert!(index < self.len(), "Cannot get at an index outside of the list bounds");
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
 
-        None
-    }
-
-
-    fn index_node(&self, element: T) -> Option<usize> {
-        unsafe {
-            let mut j = self.head?;
-            for i in 0..self.len() {
-                if (*j.as_ptr()).element == element {
-                    return Some(i)
-                }
-                j = (*j.as_ptr()).next?
-            }
-
-            None
+    #[inline]
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.len == 0 {
+          None
+        } else {
+            self.head.map(|node| unsafe {
+                let node = &*node.as_ptr();
+                self.head = node.next;
+                self.len -= 1;
+                &node.element
+            })
         }
     }
+}
 
+impl<T> IntoIterator for LinkedList<T> {
+    type Item = T;
+    type IntoIter = IntoIter<T>;
 
-    fn remove_node(&mut self, index: usize) {
-        unsafe {
-            let remove_node = match self.get_node(index) {
-                Some(node) => node,
-                None => panic!("Element was not found")
-            };
-
-            let next_node = match (*remove_node.as_ptr()).next {
-                Some(node) => node,
-                None => {
-                    if index == 0 {
-                        self.head = None
-                    } else {
-                        let pre = self.get_node(index - 1).unwrap();
-                        (*pre.as_ptr()).next = None;
-                    }
-
-                    self.len -= 1;
-                    return;
-                }
-            };
-
-            if index == 0 {
-                self.head = Some(next_node);
-            } else {
-                let pre = self.get_node(index - 1).unwrap();
-                (*pre.as_ptr()).next = Some(next_node)
-            }
-
-            self.len -= 1;
-        };
+    fn into_iter(self) -> Self::IntoIter {
+        IntoIter { list: self }
     }
 }
--- src/test/collections.rs
+++ src/test/collections.rs
@@ -1,18 +1,7 @@
 #[cfg(test)]
 mod linked_list {
-    use crate::collections::linked_list::LinkedList;
 
     #[test]
     fn base() {
-        let mut x = LinkedList::new();
-        x.push(4);
-        x.push(5);
-        x.push(6);
-
-
-        //x.remove(0);
-
-        println!("Res: {:?}", x.get(0));
-        println!("Res: {:?}", x.first());
     }
 }
